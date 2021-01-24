ab9f2211b6ee695dadd7716fe6039c6e1b2174d5
KatAnn15 <katherineviegh@gmail.com>
2021-01-24 18:48:50 +0200

    adding email credentials forms

--- ReactTest/website/src/components/login/email-login.js
+++ ReactTest/website/src/components/login/email-login.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-export default class EmailLogin extends React.Component {
+export default class EmailLoginForm extends React.Component {
   render() {
     return (
-      <div className="email-sign-up__form">
-        <input type="email" name="email" />
-        <input type="text" name="name" id="" />
-      </div>
+      <form className="email-log-in__form">
+        <input
+          className="email-log-in__input input_email"
+          type="email"
+          name="email"
+          placeholder="Email"
+        />
+        <button type="submit" className="email-log-in__submit-btn">
+          Submit
+        </button>
+      </form>
     );
   }
 }
