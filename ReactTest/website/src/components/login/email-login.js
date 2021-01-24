99dca34dc9590a866c352eb2ada31794f3914991
KatAnn15 <katherineviegh@gmail.com>
2021-01-24 16:41:44 +0200

    publish

---
+++ ReactTest/website/src/components/login/email-login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class EmailLogin extends React.Component {
+  render() {
+    return (
+      <div className="email-sign-up__form">
+        <input type="email" name="email" />
+        <input type="text" name="name" id="" />
+      </div>
+    );
+  }
+}
