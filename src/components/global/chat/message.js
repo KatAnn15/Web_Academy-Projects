cd251af2bd3859153723dd81d111426306e7a24e
KatAnn15 <katherineviegh@gmail.com>
2021-02-05 22:41:20 +0200

    fixed empty messages chat

--- src/components/global/chat/message.js
+++ src/components/global/chat/message.js
@@ -1,13 +1,10 @@
 import React from "react";
 
 export class Message extends React.Component {
-  componentDidMount = () => {
-    // console.log(this.props.attachment);
-  };
   render() {
     return (
       <div className={this.props.data}>
-        <h5 className={this.props.item}>{this.props.message}</h5>
+        {this.props.message.trim() === "" ? null : <h5 className={this.props.item}>{this.props.message}</h5>}
         {this.props.attachment ? (
           <img
             className="attachment"
